cfca99a41cf7d584a65a915219dbc62e43363e84
aripan <aripan017@gmail.com>
2020-10-07 13:53:56 +0200

    Connecting to Database

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,13 @@
 // ! ES Modules in NodeJS
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -32,6 +36,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server is running on ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running on ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 );
