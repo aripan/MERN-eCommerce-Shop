9ef95276a14be622b310863e9ae8d883c930ea4d
aripan <aripan017@gmail.com>
2020-10-07 21:28:02 +0200

    Customized Error Handlers

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
@@ -22,6 +23,12 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+//! CUSTOMIZED ERROR HANDLER
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
