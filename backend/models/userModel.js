d03f14ddb0ebf424e9b643a4f51462ba4e928c0c
aripan <aripan017@gmail.com>
2020-10-09 22:32:45 +0200

    User Registration & Password Encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,9 +30,21 @@ const userSchema = mongoose.Schema(
   }
 );
 
+// TO COMPARE PASSWORD 
 userSchema.methods.matchPassword = async function(enteredPassword){
   return await bcrypt.compare(enteredPassword, this.password)
 }
+
+// TO HASH THE ENTERED OR MODIFIED PASSWORD
+userSchema.pre("save", async function(next){
+  if(!this.isModified("password")){
+    next()
+  } else{
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+  }
+})
+
 
 const User = mongoose.model("User", userSchema);
 
