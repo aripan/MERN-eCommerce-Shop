c6f0cfa49a215458736a5d40621a39ff54b1563d
aripan <aripan017@gmail.com>
2020-10-12 23:38:17 +0200

    Added PayPal Method

--- backend/server.js
+++ backend/server.js
@@ -30,8 +30,12 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
-//! CUSTOMIZED ERROR HANDLER
+// PAYPAL
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
+//! CUSTOMIZED ERROR HANDLER
 app.use(notFound);
 
 app.use(errorHandler);
