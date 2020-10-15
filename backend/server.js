a6a5b1f2b972d0977213fba7f5a3145592f1c45f
aripan <aripan017@gmail.com>
2020-10-15 16:20:34 +0200

    Create Review Endpoint

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
@@ -21,6 +22,9 @@ connectDB();
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.json());
 
 // Home route
